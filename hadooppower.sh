9c34fd4cefcb670d82003e97b39356bf02836450
bcarson7 <bpc31@msn.com>
2016-05-17 09:28:09 -0700

    Update hadooppower.sh

--- hadooppower.sh
+++ hadooppower.sh
@@ -1,10 +1,10 @@
 yum install java-1.7.0-openjdk glibc.i686 -y
 mkdir specpower
 cd specpower
-wget http://172.19.106.68/software/benchmarking/specpower/ssj.tar
-tar -xvf ssj.tar
+wget http://172.19.106.68/software/benchmarking/specpower/ssj.zip
+unzip ssj.zip
 cd ssj
 rm -f runssj.sh
-wget  http://172.19.106.68/software/benchmarking/specpower/ceph.ssj
-chmod 775 runssj.sh
-
+wget  http://172.19.106.68/software/benchmarking/specpower/hadoop.sh
+chmod 775 hadoop.sh
+./hadoop.sh
