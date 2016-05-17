112550aa33c7777b9b9d3ed7662cc675fd922d49
bcarson7 <bpc31@msn.com>
2016-05-17 09:26:42 -0700

    Update cephpower.sh

--- cephpower.sh
+++ cephpower.sh
@@ -2,7 +2,7 @@ yum install java-1.7.0-openjdk glibc.i686 -y
 mkdir specpower
 cd specpower
 wget http://172.19.106.68/software/benchmarking/specpower/ssj.zip
-unzip ssj.tar
+unzip ssj.zip
 cd ssj
 rm -f runssj.sh
 wget  http://172.19.106.68/software/benchmarking/specpower/ceph.sh
