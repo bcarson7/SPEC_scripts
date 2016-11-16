00b71a8ed2bb52546da70461d51b5ea20f55388a
bcarson7 <bpc31@msn.com>
2016-11-16 14:52:58 -0700

    Update autostream.sh

--- autostream.sh
+++ autostream.sh
@@ -1,48 +1,7 @@
 export OMP_NUM_THREADS=1
 ./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' > stream.log
-export OMP_NUM_THREADS=2
+for i in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+do
+export OMP_NUM_THREADS=$i
 ./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=3
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=4
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=5
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=6
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=7
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=8
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=9
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=10
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=11
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=12
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=13
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=14
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=15
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=16
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=17
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=18
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=19
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=20
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=21
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=22
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=23
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
-export OMP_NUM_THREADS=24
-./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
+done
