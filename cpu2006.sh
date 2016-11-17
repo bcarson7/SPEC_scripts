dc635f712e15d7cba41fc6333185e729800e2eee
bcarson7 <bpc31@msn.com>
2016-11-17 13:46:12 -0700

    Update cpu2006.sh

--- cpu2006.sh
+++ cpu2006.sh
@@ -4,6 +4,8 @@ mkdir cpu2006
 cd cpu2006
 wget http://172.19.106.68/software/benchmarking/cpu/cpu2006.1.2.ic14.0.linux64.for.OEMs.16jan2014.tar
 wget http://172.19.106.68/software/benchmarking/cpu/cpu2006-1.2.tar
+wget http://172.19.106.68/software/benchmarking/cpu/uninstall.sh
+wget http://172.19.106.68/software/benchmarking/cpu/version.txt
 tar -xvf cpu2006-1.2.tar
 chmod 775 install.sh
 ./install.sh -f
