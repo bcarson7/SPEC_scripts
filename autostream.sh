1fa50245788993bf5693c19b8afe204024683fab
bcarson7 <bpc31@msn.com>
2016-11-22 15:27:33 -0700

    Update autostream.sh

--- autostream.sh
+++ autostream.sh
@@ -1,5 +1,5 @@
 export OMP_NUM_THREADS=1
-./streamscale | grep 'Threads\|Copy\|Scale\|Add\|Triad' > stream.log
+./stream-scale | grep 'Threads\|Copy\|Scale\|Add\|Triad' > stream.log
 for i in {2..24}
 do
 export OMP_NUM_THREADS=$i
