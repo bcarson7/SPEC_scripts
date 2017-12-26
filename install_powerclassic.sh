0eba0bacb51261d5923bfb50f1471510587544a7
bcarson7 <bpc31@msn.com>
2017-12-26 11:14:41 -0700

    Create install_powerclassic.sh

---
+++ install_powerclassic.sh
@@ -0,0 +1,5 @@
+wget http://172.19.106.68/software/powerclassic/powerclassic.zip 
+unzip powerclassic.zip
+tar -zxvf stresstest_v0.4.tar.gz
+rpm -Uvh cpuburn-1.4-1.2.el6.rf.i586.rpm
+yum -y install cpuburn
