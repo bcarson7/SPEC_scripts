975c7d53e15213c1bd071c33eecbacae414b7b69
bcarson7 <bpc31@msn.com>
2018-01-26 16:32:03 -0700

    Update score_phoronix.py

--- score_phoronix.py
+++ score_phoronix.py
@@ -3,10 +3,12 @@ import commands
 import sys
 
 PHRX_TEST = sys.argv[1]
+BENCH1 = "Apache Benchmark 2.4.7"
+BENCH2 = "PHPBench 0.8.1"
 
 lines = []
 with open(PHRX_TEST, "rt") as phrx:
         for line in phrx:
                 lines.append(line)
-print("Apache Benchmark 2.4.7 " + lines[881])
-print("PHPBench 0.8.1 " + lines[912])
+print(BENCH1 + lines[877])
+print(BENCH2 + lines[908])
