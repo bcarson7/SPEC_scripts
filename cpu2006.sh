307efd5ab29d51acb142e62d135dee6896c91e3a
bcarson7 <bpc31@msn.com>
2018-10-18 11:00:51 -0700

    Update cpu2006.sh

--- cpu2006.sh
+++ cpu2006.sh
@@ -13,12 +13,12 @@ tar -xvf cpu2006.1.2.ic14.0.linux64.for.OEMs.16jan2014.tar
 rm -f official-ws-avx2-smt-on-rate.sh
 wget http://172.19.106.68/software/benchmarking/cpu/official-ws-avx2-smt-on-rate.sh
 chmod 775 official-ws-avx2-smt-on-rate.sh
-cd config
-rm -f cpu2006.1.2.ic14.0.linux64.ws.avx2.rate.15aug2013.cfg
-wget http://172.19.106.68/software/benchmarking/cpu/Huawei_XH321_4116_256GB/cpu2006_config.zip
-unzip cpu2006_config.zip
-cd ..
-cd ..
-cp *.py cpu2006/result
-cd cpu2006
+#cd config
+#rm -f cpu2006.1.2.ic14.0.linux64.ws.avx2.rate.15aug2013.cfg
+#wget http://172.19.106.68/software/benchmarking/cpu/Huawei_XH321_4116_256GB/cpu2006_config.zip
+#unzip cpu2006_config.zip
+#cd ..
+#cd ..
+#cp *.py cpu2006/result
+#cd cpu2006
 ./official-ws-avx2-smt-on-rate.sh;./official-ws-avx2-smt-on-rate.sh;./official-ws-avx2-smt-on-rate.sh
