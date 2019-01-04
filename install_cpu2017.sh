4f823ce3e44d31b2d728f404c639cfbf3887f91e
bcarson7 <bpc31@msn.com>
2019-01-04 13:11:40 -0700

    Create install_cpu2017.sh

---
+++ install_cpu2017.sh
@@ -0,0 +1,8 @@
+wget *.tar
+yum -y update
+yum -y install gcc
+yum -y update gcc
+yum -y install gcc-c++
+yum -y update gcc-c++ 
+yum -y install libstdc++-devel-4.8.5-36.el7.x86_64
+yum -y install gcc-gfortran
