4e2bba3f9b7e5f69cec3586591a28bb42cddd839
bcarson7 <bpc31@msn.com>
2019-01-11 15:55:56 -0700

    Create get_bios_util.sh

---
+++ get_bios_util.sh
@@ -0,0 +1,6 @@
+wget http://172.19.106.68/software/benchmarking/bios_utility/sum_1.6.1_Linux_x86_64.zip
+unzip sum_1.6.1_Linux_x86_64.zip
+wget http://172.19.106.68/software/benchmarking/bios_utility/dell_toolkit.zip
+unzip dell_toolkit.zip
+wget http://172.19.106.68/software/benchmarking/bios_utility/huawei_uniCfg.zip
+unzip huawei_uniCfg.zip
