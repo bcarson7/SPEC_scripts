d3d380e8db7284fa9f1942d24340019ead493748
bcarson7 <bpc31@msn.com>
2019-01-11 11:35:19 -0700

    Update install_cpu2017.sh

--- install_cpu2017.sh
+++ install_cpu2017.sh
@@ -1,8 +1,8 @@
-wget *.tar
-yum -y update
 yum -y install gcc
-yum -y update gcc
 yum -y install gcc-c++
-yum -y update gcc-c++ 
 yum -y install libstdc++-devel-4.8.5-36.el7.x86_64
 yum -y install gcc-gfortran
+wget http://172.19.106.68/software/benchmarking/cpu/cpu2017.compiled.tar.gz
+tar -xvf cpu2017.compiled.tar.gz
+cd cpu2017
+./hope-avx512-smt-on-rate.sh
